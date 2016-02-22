1086466e909a01533f53bd18995ef13efd11f9a3
Leonardo Silveira <sombriks@gmail.com>
2016-02-22 00:33:29 -0300

    estrutura básica de leitura e carga sícnrona

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'microdadosenem',
+      user:     'postgres',
+      postgres: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
