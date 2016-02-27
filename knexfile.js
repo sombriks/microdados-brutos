f4f9a0a934d79186e068bb44152f1cc45cd3d6f6
Leonardo Silveira <sombriks@gmail.com>
2016-02-27 15:02:37 -0300

    melhorando a montagem do layout de dados

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
-
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
 
   development: {
     client: 'postgresql',
     connection: {
-      database: 'microdadosenem',
+      database: 'microdadosenem2',
       user:     'postgres',
       postgres: 'postgres'
     },
@@ -24,5 +15,4 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-
 };
