54ffc8f47df4ce9135096a01dfbc37347d0369ef
Fox <fernandodiego.sn@gmail.com>
2016-02-27 21:47:16 -0300

    layoutdb, versão quase final

---
+++ layoutdb/DADOS_ENEM_2011_ITENS.js
@@ -0,0 +1,12 @@
+module.exports = {
+  key:["DS_COR","ID_PROVA"],
+  columns:{
+    ID_POSICAO                :3,
+    COD_AREA                  :2,
+    ID_ITEM                   :5,
+    DS_GABARITO               :1,
+    ID_HABILIDADE             :2,
+    DS_COR                    :7,
+    ID_PROVA                  :3,
+  }
+};
---
+++ layoutdb/DADOS_ENEM_2014_CONSISTENCIA.js
@@ -0,0 +1,12 @@
+module.exports = {
+  key:["MASC_INSCRITO","NU_ANO"],
+  columns:{
+    ID_ENEM_ESCOLA             :6,
+    NU_INSCRICAO               :12,
+    NU_ANO                     :4,
+    COD_ESCOLA                 :8,
+    COD_UF_ESC                 :2,
+    ID_DEPENDENCIA_ADM_ESC     :1,
+    ID_LOCALIZACAO_ESC         :1,
+  }
+};
